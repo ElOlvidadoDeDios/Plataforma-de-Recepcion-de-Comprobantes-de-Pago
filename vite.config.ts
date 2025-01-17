b59c3127608499c54b2cda87df7ef6a0eab619fc
alexander hancco <184202@unsaac.edu.pe>
2025-01-17 09:17:22 -0500

    actualizacion de viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -17,4 +17,18 @@ export default defineConfig({
       ],
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://61b5-38-252-219-16.ngrok-free.app',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/login': {
+        target: 'https://80d4-38-252-219-16.ngrok-free.app',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
