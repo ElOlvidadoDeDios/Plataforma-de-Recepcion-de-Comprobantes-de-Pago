f2668ad69639a1abfd2d266c6d3f3f77bdcfee98
alexander hancco <184202@unsaac.edu.pe>
2025-09-15 10:00:37 -0500

    version 7.8  correcion de las apis de los comobox

--- src/api/afiliacionAPi.ts
+++ src/api/afiliacionAPi.ts
@@ -70,7 +70,7 @@ const sociospendientesAfiliar = async (): Promise<ApiResponse | AfiliacionSocios
 const afiliarSocioProceso = async (datos: AfiliarSocioRequest): Promise<any> => {
   try {
     const response = await fetch(
-      `${API_BASE_URL}/api_app_dile_v1_1_dev/api/afiliarSocio_proceso`,
+      `${API_BASE_URL}/api_app_dile_v1_1/api/afiliarSocio_proceso`,
       {
         method: 'POST',
         headers: {
@@ -163,7 +163,7 @@ interface datafamiliar {
 export const familiarSocioProceso = async (datos: datafamiliar): Promise<any> => {
   try {
     const response = await fetch(
-      `${API_BASE_URL}/api_app_dile_v1_1_dev/api/insertSocioFamiliar`,
+      `${API_BASE_URL}/api_app_dile_v1_1/api/insertSocioFamiliar`,
       {
         method: 'POST',
         headers: {
@@ -218,7 +218,7 @@ export interface FamiliarOpciones {
 export const useComboBoxFamiliarOpcionesData = async (): Promise<FamiliarOpciones | null> => {
   try {
     const response = await fetch(
-      `${API_BASE_URL}/api_app_dile_v1_1_dev/api/comboFamiliar`,
+      `${API_BASE_URL}/api_app_dile_v1_1/api/comboFamiliar`,
       {
         method: 'GET',
         headers: {
